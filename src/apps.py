0b9f3a42c00325399564733313086c4593d0473a
yaswanth33-ui <143922148+yaswanth33-ui@users.noreply.github.com>
2025-03-09 14:29:26 +0530

    Update apps.py
    updated name

--- src/apps.py
+++ src/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class PollsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_polls"
+    name = "django_reality_polls"
     label = "polls"
