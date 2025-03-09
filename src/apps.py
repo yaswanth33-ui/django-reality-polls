8ab38e365b04b1e58591ee6fb2a2769d728b3e07
Yaswanth Reddy <yaswanthreddypanem@gmail.com>
2025-03-09 14:53:08 +0530

    Resolve merge conflicts

--- src/apps.py
+++ src/apps.py
@@ -4,8 +4,4 @@ from django.apps import AppConfig
 class PollsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "django_reality_polls"
-<<<<<<< HEAD
     label = "polls"
-=======
-    label = "polls"
->>>>>>> origin/main
